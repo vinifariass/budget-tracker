92f0c24364b3d5c157b6238936cdda5cabcecd70
Vinicius Farias <fsvinicius9@gmail.com>
2025-04-14 17:30:21 -0300

    Creating mobile and computer navbar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,8 +1,10 @@
+import Navbar from '@/components/Navbar'
 import React, { ReactNode } from 'react'
 
 const layout = ({ children } : { children: ReactNode }) => {
     return (
         <div className='relative flex-h h-screen w-full flex-col'>
+            <Navbar />
             <div className="w-full">{children}</div>
         </div>
     )
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -12,4 +12,16 @@ const Logo = () => {
   )
 }
 
+const LogoMobile = () => {
+  return (
+    <a href='/' className='flex items-center gap-2'>
+      <PiggyBank className='stroke h-11 w-11 stroke-amber-500 stroke-[1.5]' />
+      <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        BudgetTracker
+      </p>
+    </a>
+  )
+}
+
 export default Logo
+export { LogoMobile }
